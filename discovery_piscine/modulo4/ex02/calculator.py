a5d70a037974a49d175be518f3fa97c152e12aaa
Edmilson Segredo <e.ms.segredo@gmail.com>
2026-05-22 12:21:56 +0100

    Feit

---
+++ discovery_piscine/modulo4/ex02/calculator.py
@@ -0,0 +1,8 @@
+numb1 = int(input("Give me the first number : "))
+numb2 = int(input("Give me the second number : "))
+
+print("Thank you")
+print(f'{numb1} + {numb2} = {numb1 + numb2}')
+print(f'{numb1} - {numb2} = {numb1 - numb2}')
+print(f'{numb1} / {numb2} = {int(numb1 / numb2)}')
+print(f'{numb1} x {numb2} = {numb1 * numb2}')
